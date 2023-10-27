5460b8713f349126ba623022c679be64498009d7
Ronit Pattanayak <rpatt@umich.edu>
2023-10-27 13:17:20 -0400

    delete button

--- src/components/alarmPanel.js
+++ src/components/alarmPanel.js
@@ -1,14 +1,40 @@
-import { View, Text, Switch, StyleSheet } from 'react-native'
+import { View, Text, Switch, StyleSheet, TouchableOpacity, Animated } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-export default function AlarmPanel({ hour, minutes }) {
+const renderRightActions = (progress, dragX, onDelete) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [1, 0],
+    extrapolate: 'clamp',
+  });
+
   return (
-  <View style={styles.panelContainer}> 
-    <FontAwesome style={styles.timeDigits}>
-    {hour}:{minutes}<FontAwesome style={styles.timeOfDay}>AM</FontAwesome>
-    </FontAwesome>
-    <Switch />
-  </View>
+    <TouchableOpacity onPress={onDelete}>
+      <View style={styles.deleteBox}>
+        <Animated.Text style={[styles.deleteText, { transform: [{ scale }] }]}>
+          Delete
+        </Animated.Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default function AlarmPanel({ hour, minutes, onDelete }) { // add index here 
+  onDelete = () => {
+    console.log("Delete function called.");
+  };
+  return (
+    <Swipeable
+      renderRightActions={(progress, dragX) => renderRightActions(progress, dragX, onDelete)}
+    >
+    <View style={styles.panelContainer}> 
+      <FontAwesome style={styles.timeDigits}>
+      {hour}:{minutes}<FontAwesome style={styles.timeOfDay}>AM</FontAwesome>
+      </FontAwesome>
+      <Switch />
+    </View>
+    </Swipeable>
   )
 }
 
@@ -30,6 +56,17 @@ const styles = StyleSheet.create({
   },
   timeOfDay: {
     fontSize: 20
-  }
+  },
+  deleteBox: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: '100%',
+  },
+  deleteText: {
+    color: 'white',
+    fontWeight: '700',
+  },
   
 });
