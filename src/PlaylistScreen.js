27b4cd120158364ac132faeb288c02b2c702d0f2
Eva Szeszko <eszeszko@umich.edu>
2023-11-26 22:20:53 -0500

    styled playlists + song screens

--- src/PlaylistScreen.js
+++ src/PlaylistScreen.js
@@ -1,4 +1,6 @@
-import { View, Text, Button } from 'react-native';
+import { View, Button, StyleSheet, Image} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Default from '../assets/default_playlist.png';
 
 
 const getTracks = async (uri, accessToken) => { // https://developer.spotify.com/documentation/web-api/reference/get-playlists-tracks
@@ -20,7 +22,9 @@ const getTracks = async (uri, accessToken) => { // https://developer.spotify.com
             name: song.track.name,
             artists: song.track.artists,
             href: song.track.uri,
+            img: song.track.album.images,
         }
+        //console.log(songObj.img);
         let num = songArr.push(songObj);
       })
       return songArr;
@@ -34,25 +38,59 @@ const PlaylistScreen = ({route, navigation}) => {
   let playlists = [];
   playlists = route.params.playlistArr;
   let accessToken = route.params.accessToken;
-//   playlists.map(playlist => {
-//     console.log(playlist.name)
-//   })
+
+
+  // playlists.map(playlist => {
+  //   console.log("playlist: ", playlist);
+  //   if(playlist.img[0] && playlist.img[0].length)
+  //   {
+  //     // img for playlist exists
+  //     console.log("playlist.img[0]: ", playlist.img[0].url)
+  //   }  
+  // })
 
   return (
-    <View>
+    <ScrollView>
         {playlists.map(playlist => (
-            <Button 
-            onPress={() => {
-                getTracks(playlist.tracks, accessToken).then((songsArr) => {
-                    navigation.navigate("Songs", songsArr);
-                }).catch((error) => {
-                    console.error(error);
-                  });
-            }}
-            title={playlist.name}/>
+          <View style={styles.playlists}>
+              <Button 
+              key={playlist.name}
+              onPress={() => {
+                  getTracks(playlist.tracks, accessToken).then((songsArr) => {
+                      navigation.navigate("Songs", songsArr);
+                  }).catch((error) => {
+                      console.error(error);
+                    });
+              }}
+              title={playlist.name}/>
+
+              {playlist.img && playlist.img.length > 0 && playlist.img[0].url ? (
+                <Image source={{ uri: playlist.img[0].url }} key={playlist.name} style={styles.pic} />
+                ) : (
+                  // cannot find this image:
+                  // <Image source={{ uri: "../assets/default_playlist.png" }} key={playlist.name} style={styles.pic} />
+                  null
+              )}
+
+              </View>
         ))}
-    </View>
+    </ScrollView>
   )
 };
+
+const styles = StyleSheet.create({
+  playlists: {
+    display:'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#e0e0e0'
+  },
+  pic: {
+    width: 66,
+    height: 58,
+  },
+});
 
 export default PlaylistScreen;
--- src/SongScreen.js
+++ src/SongScreen.js
@@ -1,19 +1,41 @@
-import { View, Text, Button } from 'react-native';
+import { View, Button, Image, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SongScreen = ({route, navigation}) => {
     let songArr = route.params;
-
+  
   return (
+    <ScrollView>
     <View>
         {songArr.map(song => (
+          <View style={styles.songPanel}>
             <Button
-            onPress={() => console.log(song.href)}
             key={song.name}
-            title ={song.name} />
+            title = {song.name} />
+            <Image style={styles.pic} source={{ 
+              uri: song.img[0].url, 
+            }} />
+            </View>
         ))}
     </View>
+    </ScrollView>
   )
 };
+
+const styles = StyleSheet.create({
+  songPanel: {
+    display:'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#e0e0e0'
+  },
+  pic: {
+    width: 66,
+    height: 58,
+  },
+})
 
 
 export default SongScreen;
